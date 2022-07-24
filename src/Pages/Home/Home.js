a4ae9f1ca596aadf7f0d635d066045b5f7cf2cbf
razumolla <rajuahmed9344@gmail.com>
2022-07-25 01:42:37 +0600

    Initaial project setup

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className='text-3xl'> This is Home page. Ok Daisy UI done</h1>
+        </div>
+    );
+};
+
+export default Home;
