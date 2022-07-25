fede50b931bf44edca01e551c7335fe6fed31dc7
razumolla <rajuahmed9344@gmail.com>
2022-07-25 12:57:28 +0600

    css folder

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h1 className='text-3xl'> This is Home page. Ok Daisy UI done</h1>
+            <h1 className='text-3xl'> This is Home page. Ok Daisy UI done.ok done</h1>
         </div>
     );
 };
