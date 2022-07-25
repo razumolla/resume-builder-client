4fc180bc0c2ed0f771c2935ee0a7262730ce8a5d
abdurrahaman4363 <abdur.rahaman4363@gmail.com>
2022-07-25 15:32:37 +0600

    create hired section

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Home/Banner'
 import Faq from './Faq';
+import Hired from './Hired';
 import Review from './Review';
 const Home = () => {
     return (
         <div className='m-5'>
                    
             <Banner></Banner>
+            <Hired></Hired>
             <Review></Review>
             <Faq></Faq>
         </div>
