0c04a0f11da21d31417a8bf68c1e604f95c23595
afruzaakter <afruzaaktersonia1995@gmail.com>
2022-07-25 13:13:48 +0600

    add banner section

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import Banner from '../Home/Banner'
 const Home = () => {
     return (
         <div>
             <h1 className='text-3xl'> This is Home page. Ok Daisy UI done</h1>
+            <Banner></Banner>
         </div>
     );
 };
