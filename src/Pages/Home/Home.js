f7da7ecfb544c987d3af79b95a6144e77992369a
abdurrahaman4363 <abdur.rahaman4363@gmail.com>
2022-07-25 14:10:42 +0600

    create a review component

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Review from './Review';
 
 const Home = () => {
     return (
         <div>
             <h1 className='text-3xl'> This is Home page. Ok Daisy UI done</h1>
+            <Review></Review>
         </div>
     );
 };
