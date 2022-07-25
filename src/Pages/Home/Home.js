4db13566cc033984f0f7af0ccf0ab2372a216715
razumolla <rajuahmed9344@gmail.com>
2022-07-25 13:01:08 +0600

    create new branch

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -4,6 +4,7 @@ const Home = () => {
     return (
         <div>
             <h1 className='text-3xl'> This is Home page. Ok Daisy UI done.ok done</h1>
+            <h2>ok</h2>
         </div>
     );
 };
