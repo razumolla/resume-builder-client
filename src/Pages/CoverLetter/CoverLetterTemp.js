962fed4ccdd02595c02c91e437e0f4867567c2b6
afruzaakter <afruzaaktersonia1995@gmail.com>
2022-07-28 11:47:38 +0600

    add cover page css style

--- src/Pages/CoverLetter/CoverLetterTemp.js
+++ src/Pages/CoverLetter/CoverLetterTemp.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import CoverLetterTemplate_1 from '../../assets/images/Cover-Letter-Template-1.png';
 import CoverLetterTemplate_2 from '../../assets/images/Cover-Letter-Template-2.png';
 import CoverLetterTemplate_3 from '../../assets/images/Cover-Letter-Template-3.png';
@@ -8,46 +9,83 @@ import CoverLetterTemplate_6 from '../../assets/images/Cover-Letter-Template-6.p
 import CoverLetterTemplate_7 from '../../assets/images/Cover-Letter-Template-7.jpg';
 import CoverLetterTemplate_8 from '../../assets/images/Cover-Letter-Template-8.png';
 import '../Home/Banner.css';
-
+import 'animate.css';
 const CoverLetterTemp = () => {
     return (
         <div className='m-8'>
             <div>
-                <h1 className='text-4xl font-bold mt-10 mb-10'>Select Cover Letter Template</h1>
+                <h1 className='text-4xl font-bold  mb-10'>Select Cover Letter Template</h1>
             </div>
-              <div class="  bg-gray-400  shadow-xl p-2 m-3 rounded-md">
-            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 '>
-                <div>
-                    <figure><img className='TemplateImage  rounded-md' src={CoverLetterTemplate_1} alt="Shoes" /></figure>
-                    
-                </div>
-                <div>
-                    <figure><img className='TemplateImage rounded-md' src={CoverLetterTemplate_2} alt="Shoes" /></figure>
-                </div>
-                <div>
-                    <figure><img className='TemplateImage rounded-md' src={CoverLetterTemplate_3} alt="Shoes" /></figure>
-                </div>
-                <div>
-                    <figure><img className='TemplateImage rounded-md' src={CoverLetterTemplate_4} alt="Shoes" /></figure>
-                </div>
-                <div>
-                    <figure><img className='TemplateImage rounded-md' src={CoverLetterTemplate_5} alt="Shoes" /></figure>
-                </div>
-                <div>
-                    <figure><img className='TemplateImage rounded-md' src={CoverLetterTemplate_6} alt="Shoes" /></figure>
-                </div>
-                <div>
-                    <figure><img className='TemplateImage rounded-md' src={CoverLetterTemplate_7} alt="Shoes" /></figure>
-                </div>
-                <div>
-                    <figure><img className='TemplateImage rounded-md' src={CoverLetterTemplate_8} alt="Shoes" /></figure>
-                </div>
-                <div>
-                    <figure><img className='TemplateImage rounded-md' src={CoverLetterTemplate_3} alt="Shoes" /></figure>
+              <div>
+
+            <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 justify-items-center mb-10 p-10'>
+                <div class="container animate__animated  animate__lightSpeedInLeft">
+                    <img src={CoverLetterTemplate_1} alt="" className='w-3/4 ' />
+                    <figcaption>
+                        <h3>Create Now</h3>
+                    </figcaption>
+                    <Link to=""></Link>
                 </div>
 
-            </div>
+                <div class="container animate__animated  animate__zoomIn">
+                    <img src={CoverLetterTemplate_2} alt="" className='w-3/4' />
+                    <figcaption>
+                        <h3>Create Now</h3>
+                    </figcaption>
+                    <Link to=""></Link>
+                </div>
 
+                <div class="container animate__animated  animate__lightSpeedInRight">
+                    <img src={CoverLetterTemplate_3} alt="" className='w-3/4' />
+                    <figcaption>
+                        <h3>Create Now</h3>
+                    </figcaption>
+                    <Link to=""></Link>
+                </div>
+                <div class="container animate__animated  animate__lightSpeedInRight">
+                    <img src={CoverLetterTemplate_4} alt="" className='w-3/4' />
+                    <figcaption>
+                        <h3>Create Now</h3>
+                    </figcaption>
+                    <Link to=""></Link>
+                </div>
+                <div class="container animate__animated  animate__lightSpeedInRight">
+                    <img src={CoverLetterTemplate_5} alt="" className='w-3/4' />
+                    <figcaption>
+                        <h3>Create Now</h3>
+                    </figcaption>
+                    <Link to=""></Link>
+                </div>
+                <div class="container animate__animated  animate__lightSpeedInRight">
+                    <img src={CoverLetterTemplate_6} alt="" className='w-3/4' />
+                    <figcaption>
+                        <h3>Create Now</h3>
+                    </figcaption>
+                    <Link to=""></Link>
+                </div>
+                <div class="container animate__animated  animate__lightSpeedInRight">
+                    <img src={CoverLetterTemplate_7} alt="" className='w-3/4' />
+                    <figcaption>
+                        <h3>Create Now</h3>
+                    </figcaption>
+                    <Link to=""></Link>
+                </div>
+                <div class="container animate__animated  animate__lightSpeedInRight">
+                    <img src={CoverLetterTemplate_8} alt="" className='w-3/4' />
+                    <figcaption>
+                        <h3>Create Now</h3>
+                    </figcaption>
+                    <Link to=""></Link>
+                </div>
+                <div class="container animate__animated  animate__lightSpeedInRight">
+                    <img src={CoverLetterTemplate_8} alt="" className='w-3/4' />
+                    <figcaption>
+                        <h3>Create Now</h3>
+                    </figcaption>
+                    <Link to=""></Link>
+                </div>
+            </div >
+           
 
         </div>
         </div>
