0c5690da7de3780066a3b018a85262e05e866c03
afruzaakter <afruzaaktersonia1995@gmail.com>
2022-07-29 00:03:31 +0600

    update cover letter page

--- src/Pages/CoverLetter/CoverLetter.js
+++ src/Pages/CoverLetter/CoverLetter.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import banner from '../../assets/images/cover-letter-banner 1.png';
+import banner from '../../assets/images/imgAfruza/cover-letter-banner 1.png';
 import CoverLetterTemp from './CoverLetterTemp';
 // import banner from '../../assets/images/cover-letter-banner 1.png';
 const CoverLetter = () => {
     return (
         <div >
-            <div className=" m-8 sm:bg-base-0 ">
+            <div className=" m-12 sm:bg-base-0 ">
                 <div class="hero min-h-screen ">
                     <div class="flex justify-center align-center lg:flex-row-reverse">
                         <img src={banner} class="max-w-lg rounded-lg shadow-1xl animate__zoomInDown animate__animated " />
                         <div className=''>
-                            <h1 class="text-4xl mt-36 font-bold animate__animated animate__zoomIn">Choose Cover Leter Template</h1>
-                            <p className='pr-10 animate__animated  animate__lightSpeedInLeft'>A resume is a formal document that a job applicant creates to itemize their qualifications for a position.
+                            <h1 class="text-4xl lg:text-left mt-36 font-bold animate__animated animate__zoomIn">Choose Cover Leter Template</h1>
+                            <p className='pr-10 lg:text-left animate__animated  animate__lightSpeedInLeft'>A resume is a formal document that a job applicant creates to itemize their qualifications for a position.
                         We provides the support you need to create, your resume like a professional.</p>
                            
                         </div>
