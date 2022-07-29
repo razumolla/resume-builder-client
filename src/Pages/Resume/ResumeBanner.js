a530510f4646df9277ecf0054ff9ce75b5d9abb4
syedmehedi0003 <mehedihasanbahar.com>
2022-07-29 19:22:49 +0600

    new

--- src/Pages/Resume/ResumeBanner.js
+++ src/Pages/Resume/ResumeBanner.js
@@ -7,8 +7,8 @@ const ResumeBanner = () => {
             <div class="hero-content flex-col lg:flex-row-reverse">
                 <img src={resume} class="max-w-sm rounded-lg shadow-2xl" />
                 <div className='lg:text-left text-center'>
-                    <h1 class="text-5xl font-bold text-secondary">Create Your Resume</h1>
-                    <p class="py-6">A resume is a formal document that a job applicant creates to itemize their qualifications for a position.
+                    <h1 class="text-5xl font-bold text-secondary animate__animated animate__zoomIn">Create Your Resume</h1>
+                    <p class="py-6 animate__animated  animate__lightSpeedInLeft">A resume is a formal document that a job applicant creates to itemize their qualifications for a position.
                         We provides the support you need to create, your resume like a professional.</p>
 
                 </div>
