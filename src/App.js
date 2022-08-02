a4f7a554dd90331c3781637bcff9f24f596dbcc3
rabiulhossen <rhsimon69@gmail.com>
2022-08-03 01:21:30 +0600

    notfound page created

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import Navbar from './Pages/Shared/Navbar';
-import Footer from './Pages/Shared/Footer';
-import Home from './Pages/Home/Home';
-import Templates from './Pages/Cv/Templates';
-import FormCv from './Pages/Cv/FormCv';
-import Login from './Pages/Login/Login';
-import CoverLetter from './Pages/CoverLetter/CoverLetter';
-import Resume from './Pages/Resume/Resume';
-import SignUp from './Pages/Login/SignUp';
-import TemplateForm from './Pages/CoverLetter/TemplateForm';
-import Contact from './Pages/Home/Contact';
-import About from './Pages/Home/About';
-
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./Pages/Shared/Navbar";
+import Footer from "./Pages/Shared/Footer";
+import Home from "./Pages/Home/Home";
+import Templates from "./Pages/Cv/Templates";
+import FormCv from "./Pages/Cv/FormCv";
+import Login from "./Pages/Login/Login";
+import CoverLetter from "./Pages/CoverLetter/CoverLetter";
+import Resume from "./Pages/Resume/Resume";
+import SignUp from "./Pages/Login/SignUp";
+import TemplateForm from "./Pages/CoverLetter/TemplateForm";
+import Contact from "./Pages/Home/Contact";
+import About from "./Pages/Home/About";
+import NotFound from "./Pages/Shared/NotFound";
 
 function App() {
   return (
@@ -21,19 +21,23 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
-        <Route path="/resume" element={<Resume />} ></Route>
-        <Route path='/cv' element={<Templates></Templates>}></Route>
-        <Route path='/cv/form' element={<FormCv></FormCv>}></Route>
-        <Route path='/templateForm' element={<TemplateForm></TemplateForm>}></Route>
+        <Route path="/resume" element={<Resume />}></Route>
+        <Route path="/cv" element={<Templates></Templates>}></Route>
+        <Route path="/cv/form" element={<FormCv></FormCv>}></Route>
+        <Route
+          path="/templateForm"
+          element={<TemplateForm></TemplateForm>}
+        ></Route>
         <Route path="/coverLetter" element={<CoverLetter></CoverLetter>} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/about" element={<About /> } />
-     
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound/>} />
 
+       
       </Routes>
-    
+
       <Footer></Footer>
     </div>
   );
--- src/Pages/Shared/NotFound.js
+++ src/Pages/Shared/NotFound.js
@@ -1,7 +1,25 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import error from '../../assets/images/3828537.jpg'
 export default function NotFound() {
   return (
-    <div>NotFound</div>
-  )
+    <div>
+      <div
+        class="hero min-h-screen"
+      
+      >
+          <img src={error} alt=""className="w-22" />
+        <div class=""></div>
+        <div class="hero-content text-center">
+          <div class="max-w-md">
+            <h1 class="mb-4 text-5xl font-bold shadow-md">Error Found-404</h1>
+            
+            <button class="btn btn-secondary button">
+              <Link to="/">Home</Link>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
