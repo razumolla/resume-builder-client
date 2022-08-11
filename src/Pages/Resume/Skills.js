4230924a3aae6a4a47858aca1938fd776df8d20d
syedmehedi0003 <mehedihasanbahar.com>
2022-08-12 00:20:01 +0600

    new

--- src/Pages/Resume/Skills.js
+++ src/Pages/Resume/Skills.js
@@ -4,14 +4,14 @@ const Skills = ({ formData, setFormData }) => {
     return (
         <div className='text-center'>
             <ul className="steps bg-base-300 p-3  
-                    place-items-center">
+                    place-items-center dark:bg-cyan-900">
                 <li className="step step-primary mx-4" > <p className=''>PERSONAL</p> </li>
                 <li className="step step-primary mx-4 "><p className=''>EDUCATION</p> </li>
                 <li className="step step-primary mx-4 "> <p className=''>PROJECTS</p> </li>
                 <li className="step step-primary mx-4"> <p className=''>Skills</p> </li>
             </ul>
             <div className="header">
-                <h1 className='text-2xl font-bold my-5 text center'>Enter  <span className='text-primary'> Skills </span> Info</h1>
+                <h1 className='text-2xl font-bold my-5 text center text-secondary'>Enter  <span className='text-primary'> Skills </span> Info</h1>
             </div>
 
 
