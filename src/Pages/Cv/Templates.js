38d862ee7d5c7c0d6903aa6de6042a1953a0a1b5
abdurrahaman4363 <abdur.rahaman4363@gmail.com>
2022-08-26 12:06:31 +0600

    solved the problem

--- src/Pages/Cv/Templates.js
+++ src/Pages/Cv/Templates.js
@@ -6,22 +6,8 @@ const Templates = () => {
     const [templates, setTemplates] = useState([]);
     console.log(templates);
     useEffect(() => {
-<<<<<<< HEAD
-        fetch('http://localhost:5000/cvPhoto',{
-            /*  method:'GET',
-            headers:{
-                'authorization':`Bearer ${localStorage.getItem('accessToken')}`
-            } */
-        })
-=======
-        fetch('http://localhost:5000/cvPhoto'
-            , {
-                method: 'GET',
-                headers: {
-                    'authorization': `Bearer ${localStorage.getItem('accessToken')}`
-                }
-            })
->>>>>>> 4f7432cb1730f021dee6cb3af970fe24c5c5e123
+
+        fetch('http://localhost:5000/cvPhoto')
             .then(res => res.json())
             .then(data => {
                 setTemplates(data)
@@ -33,7 +19,7 @@ const Templates = () => {
             {/* <div className="hero h-screen bg-base-100 rounded"> */}
             <div className="hero min-h-screen w-4/5 mx-auto ">
                 <div className="hero-content flex-col lg:flex-row-reverse gap-10">
-                    <img src={cvBanner} className="mx-w-full md:max-w-md m-auto rounded-lg animate__animated animate__fadeInUp animate__delay-.9s" />
+                    <img src={cvBanner} alt="" className="mx-w-full md:max-w-md m-auto rounded-lg animate__animated animate__fadeInUp animate__delay-.9s" />
                     <div className='w-48 md:w-full lg:text-left text-center'>
                         <h1 className="text-2xl md:text-5xl font-bold text-primary animate__animated animate__fadeInUp animate__delay-fast">Create Your CV</h1>
                         <p className="py-6 text-xl lg:text-2xl animate__animated animate__fadeInUp animate__delay-.8s">Our CV templates are designed with your success of mind.You can use them free.Some of them are premium.</p>
